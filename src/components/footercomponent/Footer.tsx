e57f317956596242766d596abd793a95b6c443ec
Miggorie <info@elinselin.com>
2023-01-19 10:31:19 +0100

    Solving footer stuff

--- src/components/footercomponent/Footer.tsx
+++ src/components/footercomponent/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-stone-600 px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="bg-stone-600 px-4 pt-16 mx-auto max-w-full md:px-24 lg:px-8">
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
           <a
@@ -74,36 +74,10 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-        <p className="text-sm text-gray-600">
+      <div className="flex flex-col-reverse justify-between pt-1 pb-10 border-t lg:flex-row">
+        <p className="text-sm text-stone-200 mt-4">
           © Copyright 2020 Lorem Inc. All rights reserved.
         </p>
-        <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-          <li>
-            <a
-              href="/"
-              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-            >
-              F.A.Q
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-            >
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-            >
-              Terms &amp; Conditions
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
   );
