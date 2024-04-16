f01347dafa0a0c788ca35f914d1d365136eb2abe
mingman <ingman.m@gmail.com>
2024-04-16 13:07:39 +0200

    adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       backgroundImage: {
         hero: "url('../src/images/hundgrid.jpg')",
       },
+      colors: {
+        darktext: "#374151",
+        lightcolor: "#e7e5e4",
+        lightbrown: "#c9c3bb",
+        lighterbrown: "#e1ded8",
+      },
     },
   },
   plugins: [
